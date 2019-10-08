721b863a754ac171cd6858c2ca90090991401964
MohammadSohail <sohailnsu15@gmail.com>
2019-10-08 12:42:37 +0600

    JSON data model

--- js/script.js
+++ js/script.js
@@ -1,7 +1,68 @@
+var users = [
+    {
+        name: 'Lucy',
+        gender: 'F',
+        hobby: 'pets',
+        avatar: 'avatar1.png'        
+    },
+    {
+        name: 'Betty',
+        gender: 'F',
+        hobby: 'pets',
+        avatar: 'avatar2.png'
+    },
+    {
+        name: 'Ronald',
+        gender: 'M',
+        hobby: 'music',
+        avatar: 'avatar3.png'
+    },
+    {
+        name: 'Christopher',
+        gender: 'M',
+        hobby: 'sports',
+        avatar: 'avatar4.png'
+    },
+    {
+        name: 'Ximena',
+        gender: 'F',
+        hobby: 'reading',
+        avatar: 'avatar5.png'
+    },
+    {
+        name: 'Paul',
+        gender: 'M',
+        hobby: 'shopping',
+        avatar: 'avatar6.png'
+    },
+    {
+        name: 'Charlie',
+        gender: 'M',
+        hobby: 'pets',
+        avatar: 'avatar7.png'
+    },
+];
+
+
+
 window.addEventListener('load',function() {
-    
+    var result = this.document.getElementById('results');
     function search(){
-        console.log('Seaching!');
+        //get hobby
+        var hobbyField = document.getElementById('hobby');
+        var hobby = hobbyField.value;
+        console.log(hobby);
+
+        //get gender
+        var genderField = document.getElementById('gender');
+        var s = genderField.selectedIndex;
+        var gender = genderField.options[s].value;
+        console.log(gender);
+        var st = '';
+        for(var i = 0; i < users.length; i++){
+            st += ' ' + users[i].name;
+        }
+        results.innerHTML = st;
     }
     var searchBtn = this.document.getElementById('searchBtn');
     searchBtn.addEventListener('click', search);
